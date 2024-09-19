584dc3f834dad7786a3b19ef7979d7498aa639b3
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-20 03:19:33 +0500

    completed

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import CountdownTimerApp from "@/components/counter/Counter";
+
+export default function Home() {
+    return (
+        <>
+
+            <CountdownTimerApp />
+
+        </>
+    )
+}
